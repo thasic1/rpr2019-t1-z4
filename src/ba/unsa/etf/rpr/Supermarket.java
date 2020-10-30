250e32d13ff66f2270fc751a99a495bb981bd459
thasic1 <thasic1@etf.unsa.ba>
2020-10-30 09:33:28 +0100

    Zavrseno!!!

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 
 public class Supermarket {
     ArrayList<Artikl> artikli = new ArrayList<Artikl>();
-    public boolean dodajArtikl(Artikl a){
+    public void dodajArtikl(Artikl a){
         if(artikli.size()<50){
             artikli.add(a);
-            return true;
         }
-        return false;
     }
     public Artikl izbaciArtiklSaKodom(String kod){
         for(int i=0;i<artikli.size();i++){
